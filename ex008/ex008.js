e7f8d3e95b587019c9191e39bee4ca0342aead6b
Ruan Pablo Rebouças dos Santos <106494798+ruanpreboucas@users.noreply.github.com>
2023-01-29 15:23:29 -0300

    corrigindo erro de digitação

--- ex008/ex008.js
+++ ex008/ex008.js
@@ -1,8 +1,8 @@
 var vel = 78.2
 
-console.log(`A velocidade do seu carro é ${vel}`)
+console.log(`A velocidade do seu carro é ${vel}km/h`)
 
 if (vel > 60){
-    console.log('Você iultrapassou o limite de velocidade. MULTADO!')
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
 }//Condição simples
 console.log(`Dirija sempre usando o cinto de segurança`)
