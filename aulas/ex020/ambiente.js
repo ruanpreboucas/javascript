5b0528f8d4114775df9ce14e95b259e0b67862f7
ruanpreboucas <ruan.santos53@etec.sp.gov.br>
2023-07-12 17:13:13 -0300

    aprendendo sobre vetores

---
+++ aulas/ex020/ambiente.js
@@ -0,0 +1,7 @@
+ let num = [5,8,9]
+ 
+ num.sort()
+ num.push(47)
+ console.log(`Nosso vetor é o ${num}`) 
+ console.log(`O vetor tem ${num.length} posições`) 
+ console.log(`O primeiro elemento do vetor o ${num[0]}`) 
---
+++ aulas/ex020/vetor.js
@@ -0,0 +1,21 @@
+let valores = [8, 1, 7, 4, 3, 54, 29, -1]
+
+
+/*for (let c=0; c <=4; c++){
+    console.log(valores[c])
+}*/ // valores do vetor até a posição 5
+
+console.log(valores)
+ 
+/*for (let c=0; c < valores.length; c++){
+    console.log(`o item da posição ${c} tem valor ${valores[c]}`)
+}
+*/
+
+/*for (c in valores){
+    console.log(`o item da posição ${c} tem valor ${valores[c]}`)
+}
+*/
+
+let c = valores.indexOf(9)
+console.log(`o valor está na posição ${c}`)
